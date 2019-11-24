4703b1045965bcf0d77e7ece6dabe652ed515f01
seapeak <dashg@hotmail.com>
2019-11-24 18:18:02 -0500

    Add comments in m file

--- DecompositionAndVB.m
+++ DecompositionAndVB.m
@@ -1,7 +1,14 @@
+
 clear all
 clc
 A=[1.8 0.6 -0.2;0.8 1.6 -0.2;-0.4 -0.8 2.6];
 B=[1;1;0];
+%%(A-lemta*I)W=0;
+%%    [V,D] = eig(A) produces a diagonal matrix D of eigenvalues and 
+%%     [V,D] = eig(A) produces a diagonal matrix D of eigenvalues and 
+%%     a full matrix V whose columns are the corresponding eigenvectors  
+%%     so that A*V = V*D.    a full matrix V whose columns are the corresponding%%  eigenvectors  
+%%     so that A*V = V*D.
 [W,1]=eig(A);
 V=inv(W);
 VB=v*B;
